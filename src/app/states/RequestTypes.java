95fef80727e3442b0ec8540dd54ab89cf5ca1377
LIVIU CHIRCU <liviu.chircu@cti.pub.ro>
2013-04-17 08:59:34 +0300

    Moving to GitHub :)

---
+++ src/app/states/RequestTypes.java
@@ -0,0 +1,30 @@
+package app.states;
+
+/**
+ * List of the possible contextual menu items of the application
+ * 
+ * @author Andreea Hodea & Liviu Chircu
+ * 
+ */
+public class RequestTypes {
+
+	/* GUI context menu specific requests */
+	public static final int REQUEST_LAUNCH_OFFER = 0;
+	public static final int REQUEST_DROP_OFFER = 1;
+	public static final int REQUEST_MAKE_OFFER = 2;
+	public static final int REQUEST_ACCEPT_OFFER = 3;
+	public static final int REQUEST_REFUSE_OFFER = 4;
+	public static final int REQUEST_DROP_AUCTION = 5;
+
+	/* internally generated requests */
+	public static final int REQUEST_INITIAL_TRANSFER = 6;
+	public static final int REQUEST_TRANSFER = 7;
+
+	/* WS related actions */
+	public static final int REQUEST_LOGIN = 8;
+	public static final int REQUEST_LOGOUT = 9;
+	public static final int REQUEST_RELEVANT_USERS = 10;
+	public static final int SYSTEM_LOGIN_FAILURE = 11;
+	public static final int SYSTEM_NEW_LOGIN_EVENT = 12;
+	
+}
